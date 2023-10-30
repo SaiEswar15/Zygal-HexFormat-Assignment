e1198f72de25c46a56a8d1dba973869f659532f0
SaiEswar15 <saieswarkumar@gmail.com>
2023-10-30 12:15:31 +0530

    update

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+const canvas = document.getElementById("myCanvas");
+const context = canvas.getContext("2d");
 
 
-ctx.font = "12px Arial"; 
-ctx.fillStyle = "red"; 
+context.font = "12px Arial"; 
+context.fillStyle = "red"; 
 
 
-var text = "S"; 
-var textWidth = ctx.measureText(text).width;
-var x = (canvas.width - textWidth) / 2;
-var y = canvas.height / 2;
+const text = "S"; 
+const textWidth = context.measureText(text).width;
+const x = (canvas.width - textWidth) / 2;
+const y = canvas.height / 2;
 
 
-ctx.fillText(text, x, y);
+context.fillText(text, x, y);
 
 
-var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-var pixels = imageData.data;
+const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+const pixels = imageData.data;
 
-var hexData = [];
-for (var i = 0; i < pixels.length; i += 4) {
+const hexData = [];
+for (const i = 0; i < pixels.length; i += 4) {
     
-    var r = pixels[i];
-    var g = pixels[i + 1];
-    var b = pixels[i + 2];
+    const r = pixels[i];
+    const g = pixels[i + 1];
+    const b = pixels[i + 2];
     
     
-    var hexColor = "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
+    const hexColor = "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
     
     hexData.push(hexColor);
 }
 
 
-var hexText = hexData.join("\n");
+const hexText = hexData.join("\n");
 
 
-var blob = new Blob([hexText], { type: "text/plain" });
+const blob = new Blob([hexText], { type: "text/plain" });
 
 
-var downloadLink = document.getElementById("downloadLink");
+const downloadLink = document.getElementById("downloadLink");
 downloadLink.href = URL.createObjectURL(blob);
